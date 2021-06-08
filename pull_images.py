7a99642f4e7b20352329a671fef67a17a91c5a33
Xevryn <Xevryn@gmail.com>
2021-06-07 22:21:55 -0600

    Removed image pull that didn't work

--- pull_images.py
+++ pull_images.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
             if xObject[obj]['/Subtype'] == '/Image':
                 size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
                 data = xObject[obj].getData()
-                #for i in xObject[obj]:
-                #    print(f"{i} : {xObject[obj][i]}")
-                print(f"")
-                print(f"{data}")
+                for i in xObject[obj]:
+                   print(f"{i} : {xObject[obj][i]}")
+                # print(f"")
+                # print(f"{data}")
                 if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
                     mode = "RGB"
                 else:
@@ -38,48 +38,6 @@ if __name__ == '__main__':
                     img.write(data)
                     img.close()
 
-# 2  Could potentially work, but the below is for jpg only.  Not sure what the startmark and endmark are for non jpg images
-#import sys
-#
-#pdf = open(sys.argv[1], "rb").read()
-#with open('testfile.out', 'wb') as writefile:
-#    writefile.write(pdf)
-#
-#startmark = "\xff\xd8"
-#startfix = 0
-#endmark = "\xff\xd9"
-#endfix = 2
-#i = 0
-#
-#njpg = 0
-#while True:
-#    #istream = pdf.find("stream", i)
-#    istream = pdf.find(str.encode("stream"), i)
-#    if istream < 0:
-#        break
-#    istart = pdf.find(str.encode(startmark), istream, istream+20)
-#    if istart < 0:
-#        i = istream+20
-#        continue
-#    iend = pdf.find("endstream", istart)
-#    if iend < 0:
-#        raise Exception("Didn't find end of stream!")
-#    iend = pdf.find(endmark, iend-20)
-#    if iend < 0:
-#        raise Exception("Didn't find end of JPG!")
-#    
-#    istart += startfix
-#    iend += endfix
-#    print(f"JPG {njpg} from {istart} to {iend}")
-#    jpg = pdf[istart:iend]
-#    filename = 'jpg'+njpg+'.jgp'
-#    #jpgfile = file("jpg%d.jpg" % njpg, "wb")
-#    jpgfile = open(filename,  "wb")
-#    jpgfile.write(jpg)
-#    jpgfile.close()
-#    
-#    njpg += 1
-#    i = iend
 
 # 3
 import PyPDF2
